868876679b087daa3c0ecab4823a1d06e551c3d3
Rodney Osodo <socials@rodneyosodo.com>
2024-06-18 23:55:31 +0300

    feat(burn): Add burn's mnist model
    Add mnist image classification model from burn's documentation
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

---
+++ burn-algorithms/mnist/src/main.rs
@@ -0,0 +1,34 @@
+#[cfg(feature = "ndarray")]
+mod ndarray {
+    use burn::backend::{
+        ndarray::{NdArray, NdArrayDevice},
+        Autodiff,
+    };
+    use mnist::training;
+
+    pub fn run() {
+        let device = NdArrayDevice::Cpu;
+        training::run::<Autodiff<NdArray>>(device);
+    }
+}
+
+#[cfg(feature = "wgpu")]
+mod wgpu {
+    use burn::backend::{
+        wgpu::{Wgpu, WgpuDevice},
+        Autodiff,
+    };
+    use mnist::training;
+
+    pub fn run() {
+        let device = WgpuDevice::default();
+        training::run::<Autodiff<Wgpu>>(device);
+    }
+}
+
+fn main() {
+    #[cfg(feature = "ndarray")]
+    ndarray::run();
+    #[cfg(feature = "wgpu")]
+    wgpu::run();
+}
---
+++ burn-algorithms/mnist/src/training.rs
@@ -0,0 +1,101 @@
+use crate::{data::MnistBatcher, model::Model};
+
+use burn::{
+    data::{dataloader::DataLoaderBuilder, dataset::vision::MnistDataset},
+    optim::{decay::WeightDecayConfig, AdamConfig},
+    prelude::*,
+    record::{CompactRecorder, NoStdTrainingRecorder},
+    tensor::backend::AutodiffBackend,
+    train::{
+        metric::{
+            store::{Aggregate, Direction, Split},
+            AccuracyMetric, CpuMemory, CpuTemperature, CpuUse, LossMetric,
+        },
+        LearnerBuilder, MetricEarlyStoppingStrategy, StoppingCondition,
+    },
+};
+
+static ARTIFACT_DIR: &str = "artifacts/";
+
+#[derive(Config)]
+pub struct MnistTrainingConfig {
+    #[config(default = 10)]
+    pub num_epochs: usize,
+
+    #[config(default = 64)]
+    pub batch_size: usize,
+
+    #[config(default = 4)]
+    pub num_workers: usize,
+
+    #[config(default = 42)]
+    pub seed: u64,
+
+    pub optimizer: AdamConfig,
+}
+
+fn create_artifact_dir(artifact_dir: &str) {
+    // Remove existing artifacts before to get an accurate learner summary
+    std::fs::remove_dir_all(artifact_dir).ok();
+    std::fs::create_dir_all(artifact_dir).ok();
+}
+
+pub fn run<B: AutodiffBackend>(device: B::Device) {
+    create_artifact_dir(ARTIFACT_DIR);
+    // Config
+    let config_optimizer = AdamConfig::new().with_weight_decay(Some(WeightDecayConfig::new(5e-5)));
+    let config = MnistTrainingConfig::new(config_optimizer);
+    B::seed(config.seed);
+
+    // Data
+    let batcher_train = MnistBatcher::<B>::new(device.clone());
+    let batcher_valid = MnistBatcher::<B::InnerBackend>::new(device.clone());
+
+    let dataloader_train = DataLoaderBuilder::new(batcher_train)
+        .batch_size(config.batch_size)
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(MnistDataset::train());
+    let dataloader_test = DataLoaderBuilder::new(batcher_valid)
+        .batch_size(config.batch_size)
+        .shuffle(config.seed)
+        .num_workers(config.num_workers)
+        .build(MnistDataset::test());
+
+    // Model
+    let learner = LearnerBuilder::new(ARTIFACT_DIR)
+        .metric_train_numeric(AccuracyMetric::new())
+        .metric_valid_numeric(AccuracyMetric::new())
+        .metric_train_numeric(CpuUse::new())
+        .metric_valid_numeric(CpuUse::new())
+        .metric_train_numeric(CpuMemory::new())
+        .metric_valid_numeric(CpuMemory::new())
+        .metric_train_numeric(CpuTemperature::new())
+        .metric_valid_numeric(CpuTemperature::new())
+        .metric_train_numeric(LossMetric::new())
+        .metric_valid_numeric(LossMetric::new())
+        .with_file_checkpointer(CompactRecorder::new())
+        .early_stopping(MetricEarlyStoppingStrategy::new::<LossMetric<B>>(
+            Aggregate::Mean,
+            Direction::Lowest,
+            Split::Valid,
+            StoppingCondition::NoImprovementSince { n_epochs: 1 },
+        ))
+        .devices(vec![device.clone()])
+        .num_epochs(config.num_epochs)
+        .summary()
+        .build(Model::new(&device), config.optimizer.init(), 1e-4);
+
+    let model_trained = learner.fit(dataloader_train, dataloader_test);
+
+    config
+        .save(format!("{ARTIFACT_DIR}/config.json").as_str())
+        .unwrap();
+
+    model_trained
+        .save_file(
+            format!("{ARTIFACT_DIR}/model"),
+            &NoStdTrainingRecorder::new(),
+        )
+        .expect("Failed to save trained model");
+}

fa249d978e0b596c1f1b0c9e369f51c5954e5812
Rodney Osodo <socials@rodneyosodo.com>
2024-06-25 11:29:01 +0300

    fix(cifar-10): Seperate data download from model training

--- burn-algorithms/README.md
+++ burn-algorithms/README.md
@@ -7,3 +7,31 @@
 - [x] Image Classification - (CIFAR-10 dataset)
 - [x] Text Classification - (AG News dataset)
 - [x] Text Classification - (IMDB dataset)
+
+## Cifar-10 dataset
+
+First, download the dataset from <https://s3.amazonaws.com/fast-ai-sample/cifar10.tgz> and extract it in the `data` folder inside the `cifar10` folder.
+
+Download the dataset
+
+```bash
+wget https://s3.amazonaws.com/fast-ai-sample/cifar10.tgz -P cifar10/data
+```
+
+Extract the dataset
+
+```bash
+tar -xvzf cifar10/data/cifar10.tgz -C cifar10/data
+```
+
+Then, run the following command to train the model on wgpu or ndrray.
+
+```bash
+cargo run --bin cifar10 --features wgpu
+```
+
+or
+
+```bash
+cargo run --bin cifar10 --features ndrray
+```
--- burn-algorithms/cifar10/src/dataset.rs
+++ burn-algorithms/cifar10/src/dataset.rs
@@ -1,19 +1,6 @@
-use flate2::read::GzDecoder;
-use std::path::{Path, PathBuf};
-use tar::Archive;
-
 use burn::data::dataset::vision::ImageFolderDataset;
-use burn_common::network::downloader::download_file_as_bytes;
+use std::path::{Path, PathBuf};
 
-/// CIFAR-10 mirror from [fastai](https://github.com/fastai/fastai/blob/master/fastai/data/external.py#L44).
-/// Licensed under the [Appache License](https://github.com/fastai/fastai/blob/master/LICENSE).
-const URL: &str = "https://s3.amazonaws.com/fast-ai-sample/cifar10.tgz";
-
-/// The [CIFAR-10](https://www.cs.toronto.edu/%7Ekriz/cifar.html) dataset consists of 60,000 32x32
-/// colour images, with 6,000 images per class. There are 50,000 training images and 10,000 test
-/// images.
-///
-/// The data is downloaded from the web from the [fastai mirror](https://github.com/fastai/fastai/blob/master/fastai/data/external.py#L44).
 pub trait CIFAR10Loader {
     fn cifar10_train() -> Self;
     fn cifar10_test() -> Self;
@@ -21,29 +8,25 @@ pub trait CIFAR10Loader {
 
 impl CIFAR10Loader for ImageFolderDataset {
     fn cifar10_train() -> Self {
-        let root = download();
+        let root = data_path();
 
         Self::new_classification(root.join("train")).unwrap()
     }
 
     fn cifar10_test() -> Self {
-        let root = download();
+        let root = data_path();
 
         Self::new_classification(root.join("test")).unwrap()
     }
 }
 
-fn download() -> PathBuf {
+fn data_path() -> PathBuf {
     let example_dir = Path::new(file!()).parent().unwrap().parent().unwrap();
-    let cifar_dir = example_dir.join("cifar10");
+    let cifar_dir = example_dir.join("data/cifar10");
 
     let labels_file = cifar_dir.join("labels.txt");
     if !labels_file.exists() {
-        let bytes = download_file_as_bytes(URL, "cifar10.tgz");
-
-        let gz_buffer = GzDecoder::new(&bytes[..]);
-        let mut archive = Archive::new(gz_buffer);
-        archive.unpack(example_dir).unwrap();
+        panic!("Downloading CIFAR-10 dataset...");
     }
 
     cifar_dir

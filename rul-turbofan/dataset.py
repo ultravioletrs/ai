ebd247a6686071078da17da40c7962bfab24b9f1
filip <filipcivljak007@gmail.com>
2024-07-01 16:42:36 +0200

    RUL_Turbofan deletes datasets

--- rul-turbofan/dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-column_names = [
-    "unit_number", "time_in_cycles", "operational_setting_1", "operational_setting_2", "operational_setting_3"
-] + [f"sensor_measurement_{i}" for i in range(1, 27)]
-
-df_train = pd.read_csv("train_FD001.txt", sep=r'\s+', header=None, names=column_names)
-
-print(df_train.head(20))

5e4483c06ea8da3fbdc057d901cac4ea6102575d
Rodney Osodo <socials@rodneyosodo.com>
2024-07-03 16:19:51 +0300

    feat(wasm): Add matrix addition
    Add matrix addition to be done on wasm using burn machine learning framework
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

---
+++ burn-algorithms/addition-wasm-inference/src/main.rs
@@ -0,0 +1,34 @@
+// Copied from: https://github.com/ultravioletrs/ai/blob/main/burn-algorithms/addition/src/main.rs
+
+use burn::{
+    prelude::*,
+    tensor::{backend::Backend, Tensor},
+};
+use futures::executor;
+use serde_json;
+
+pub async fn addition<B: Backend>(
+    a: [[f32; 2]; 2],
+    b: [[f32; 2]; 2],
+) -> Data<<B as Backend>::FloatElem, 2> {
+    let device = Default::default();
+
+    let tensor1: Tensor<B, 2> = Tensor::from_floats(a, &device);
+    let tensor2: Tensor<B, 2> = Tensor::from_floats(b, &device);
+
+    let result = tensor1 + tensor2;
+
+    #[cfg(not(target_family = "wasm"))]
+    return result.into_data();
+
+    #[cfg(target_family = "wasm")]
+    return result.into_data().await;
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let a: [[f32; 2]; 2] = serde_json::from_str(&args[1]).unwrap();
+    let b: [[f32; 2]; 2] = serde_json::from_str(&args[2]).unwrap();
+    let result: Data<f32, 2> = executor::block_on(addition::<burn::backend::NdArray>(a, b));
+    println!("{:}", result);
+}

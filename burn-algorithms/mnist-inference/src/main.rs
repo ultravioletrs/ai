c621c126277d24e14bfa5447fa24917559bba0fd
Rodney Osodo <socials@rodneyosodo.com>
2024-07-19 15:50:47 +0300

    fix: Remove `expect` which results to panic
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- burn-algorithms/mnist-inference/src/main.rs
+++ burn-algorithms/mnist-inference/src/main.rs
@@ -7,8 +7,16 @@ fn main() {
         eprintln!("Usage: {} <JSON input>", args[0]);
         std::process::exit(1);
     }
-    let input: Vec<f32> = serde_json::from_str(&args[1])
-        .expect("Provide a valid JSON input for example: [0.0, 0.0, ..., 0.0]");
+    let input: Vec<f32> = match serde_json::from_str(&args[1]) {
+        Ok(a) => a,
+        Err(e) => {
+            eprintln!(
+                "Provide a valid JSON input for example: [0.0, 0.0, ..., 0.0]: {}",
+                e
+            );
+            std::process::exit(1);
+        }
+    };
     let result = executor::block_on(inference(input.as_slice()));
     println!("{}", result);
 }
--- burn-algorithms/winequality-inference/src/main.rs
+++ burn-algorithms/winequality-inference/src/main.rs
@@ -7,7 +7,13 @@ fn main() {
         eprintln!("Usage: {} <JSON input>", args[0]);
         std::process::exit(1);
     }
-    let deserialized: WineQualityItem = serde_json::from_str(&args[1]).expect("Provide a valid JSON input for example: {\"fixed_acidity\": 7,\"volatile_acidity\": 0.27,\"citric_acid\": 0.36,\"residual_sugar\": 20.7,\"chlorides\": 0.045,\"free_sulfur_dioxide\": 45,\"total_sulfur_dioxide\": 170,\"density\": 1.001,\"ph\": 3,\"sulphates\": 0.45,\"alcohol\": 8.8}");
+    let deserialized: WineQualityItem = match serde_json::from_str(&args[1]) {
+        Ok(a) => a,
+        Err(e) => {
+            eprintln!("Provide a valid JSON input for example: {{\"fixed_acidity\": 7,\"volatile_acidity\": 0.27,\"citric_acid\": 0.36,\"residual_sugar\": 20.7,\"chlorides\": 0.045,\"free_sulfur_dioxide\": 45,\"total_sulfur_dioxide\": 170,\"density\": 1.001,\"ph\": 3,\"sulphates\": 0.45,\"alcohol\": 8.8}}: {}", e);
+            std::process::exit(1);
+        }
+    };
     match executor::block_on(inference(deserialized)) {
         Ok(result) => println!("{}", result),
         Err(e) => eprintln!("{}", e),
